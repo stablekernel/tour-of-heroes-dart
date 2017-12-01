ff08aa62b944b22d88617fae405a0aba93b4ba56
Joe Conway <joeconway3@gmail.com>
2017-11-30 20:27:31 -0500

    initial commit

---
+++ web/main.dart
@@ -0,0 +1,29 @@
+import 'package:angular/angular.dart';
+import 'package:angular_router/angular_router.dart';
+import 'package:angular_tour_of_heroes/app_component.dart';
+import 'package:angular_tour_of_heroes/in_memory_data_service.dart';
+import 'package:http/http.dart';
+import 'package:http/browser_client.dart';
+
+void main() {
+  bootstrap(AppComponent, [
+    ROUTER_PROVIDERS,
+    // Remove next line in production
+    provide(LocationStrategy, useClass: HashLocationStrategy),
+    // Using a real back end?
+    // Import browser_client.dart and change the above to:
+    provide(Client, useFactory: () => new BrowserClient(), deps: [])
+  ]);
+}
+/*
+import 'package:http/browser_client.dart';
+
+void main() {
+  bootstrap(AppComponent, [
+    ROUTER_PROVIDERS,
+    // Remove next line in production
+    provide(LocationStrategy, useClass: HashLocationStrategy),
+    provide(BrowserClient, useFactory: () => new BrowserClient(), deps: [])
+  ]);
+}
+*/
